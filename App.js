be28db58e617af64813487b54aa0d9fb2fe00c3d
vursha <97270930+vursha@users.noreply.github.com>
2022-05-27 16:02:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import StackNavi from './navigation/StackNavi';
+
+
+
+export default function App() {
+ 
+  return (
+    <StackNavi/>
+  );
+}
